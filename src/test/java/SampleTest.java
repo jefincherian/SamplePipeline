fb12bfac667f1bb60452a2b5f9420a836f5aff50
Jefin <jefin@qburst.com>
2023-01-16 23:21:40 +0530

    First commit

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,29 @@
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testcase_1() {
+        System.out.println("Executed test case 1");
+    }
+
+    @Test
+    public void testcase_2() {
+        System.out.println("Executed test case 2");
+    }
+
+    @Test
+    public void testcase_3() {
+        System.out.println("Executed test case 3");
+    }
+
+    @Test
+    public void testcase_4() {
+        System.out.println("Executed test case 4");
+    }
+
+    @Test
+    public void testcase_5() {
+        System.out.println("Executed test case 5");
+    }
+}
